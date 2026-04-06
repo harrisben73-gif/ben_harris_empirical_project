4d357c690659eae8fcc73ad7a1515a8a55eb5819
harrisben73-gif <harrisben73@gmail.com>
2026-04-06 16:41:20 +0100

    Worked out how to create linux folders and files and will now use this to clean out the goalkeeper data, which will allow for easier analysis.

---
+++ goalkeeper_analysis/cleaning.sh
@@ -0,0 +1,12 @@
+# Removing Duplicates 
+sort data/raw/pl_dataset_2526_gkstats.csv | uniq > temp.csv 
+
+# Removing empty rows 
+grep -v '^$' temp.csv > data/processed/gkstats_clean.csv 
+
+# Remove temp file 
+rm temp.csv 
+
+echo "Cleaning complete!"
+
+
