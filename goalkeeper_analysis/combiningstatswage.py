88904d92cde65a8a734486bb5194da6574f62cdf
harrisben73-gif <harrisben73@gmail.com>
2026-04-08 23:32:05 +0100

    Finalising my data cleaning of goalkeeper_analysis by making sure all the data are integers and not strings and checking this with some common commands.

--- goalkeeper_analysis/combiningstatswage.py
+++ goalkeeper_analysis/combiningstatswage.py
@@ -2,11 +2,21 @@
 
 import pandas as pd 
 
-wages = pd.read_csv('data/processed/gk_wages.csv')
-stats = pd.read_csv('data/processed/gkstats_clean.csv')
+wages = pd.read_csv('goalkeeper_analysis/data/processed/gk_wages.csv')
+stats = pd.read_csv('goalkeeper_analysis/data/processed/gkstats_clean.csv')
 
 # Merging the two datasets on the 'Player' column to create a combined dataset that includes both stats and wage information for each goalkeeper
 
-combined = pd.merge(stats, wages, on='Player', how='inner')
-combined.to_csv('data/clean/gk_combined.csv', index=False)
+combined = pd.merge(stats, wages, on='Player', how='left')
+combined.to_csv('goalkeeper_analysis/data/clean/gk_combined.csv', index=False)
 
+# Dropping unwanted columns 
+combined = combined.drop(columns=['Rank_y', 'Nation_y', 'Position_y', 'Squad_y', 'Age_y'])
+
+# Modifying the wage columns so that it just shows it in GBP or £
+combined['Weekly Wages'] = (combined['Weekly Wages'].astype(str).str.extract(r'£\s*([\d,]+)').iloc[:, 0].str.replace(',', '', regex = False).astype(float))
+combined['Annual Wages'] = (combined['Annual Wages'].astype(str).str.extract(r'£\s*([\d,]+)').iloc[:, 0].str.replace(',', '', regex = False).astype(float))
+
+combined.to_csv('goalkeeper_analysis/data/clean/gk_combined.csv', index=False)
+
+# Data is now cleaned and combined, I will be using this dataset for my analysis in the gk_data_analysis.py file
--- goalkeeper_analysis/gk_data_analysis.py
+++ goalkeeper_analysis/gk_data_analysis.py
@@ -4,53 +4,33 @@ import pandas as pd
 import sqlite3
 
 # Creating a connection to the SQLite database where the data is stored
-conn = sqlite3.connect('sqlite/gk_database.db')
-
 # Reading in the data from the database into pandas dataframes for analysis
 df = pd.read_csv('goalkeeper_analysis/data/clean/gk_combined.csv')
+df = df.dropna()
+conn = sqlite3.connect('goalkeeper_analysis/sqlite/gk_database.db')
 df.to_sql('gk_combinedsql', conn, if_exists='replace', index=False)
 
-# Final bit of data cleaning before analysis, converting the "Weekly Wages" column to numeric values for easier calculations
-df['weekly_wages_clean'] = (df['Weekly Wages'].str.replace(r"[£,]", "", regex=True).str.extract(r"(\d+)").astype(float))
-df.to_sql('gk_combinedsql', conn, if_exists='replace', index=False)
 
 # Printing the first 5 rows of the SQL table to verify it has been created correctly
 # print(pd.read_sql_query("SELECT * FROM gk_combinedsql LIMIT 5", conn))
 
 # Finding the average wage of all the goalkeepers in the dataset
-avg_wage = pd.read_sql_query("""SELECT AVG(weekly_wages_clean) AS avg_wage FROM gk_combinedsql""", conn)
+avg_wage = pd.read_sql_query("""SELECT AVG("Weekly Wages") AS avg_wage FROM gk_combinedsql""", conn)
 print(f"The average weekly wage of the goalkeepers in the dataset is: £{round(avg_wage['avg_wage'].iloc[0])}")
 
+print(df['Weekly Wages'].head())
+
+
 # Finding the top 10 highest paid goalkeepers in the dataset
-top_paid_gks = pd.read_sql_query("""SELECT Player, Squad_x, weekly_wages_clean FROM gk_combinedsql ORDER BY weekly_wages_clean DESC LIMIT 10""", conn)
+top_paid_gks = pd.read_sql_query("""SELECT Player, Squad_x, "Weekly Wages" FROM gk_combinedsql ORDER BY "Weekly Wages" DESC LIMIT 10""", conn)
 print("The top 10 highest paid goalkeepers in the dataset are:", top_paid_gks.values.tolist())
 
 # Finding the top 10 goalkeepers by save percentage in the dataset
-top_save_percentage_gks = pd.read_sql_query("""SELECT Player, Squad_x, 'Save Percentage' FROM gk_combinedsql ORDER BY 'Save Percentage' DESC LIMIT 10""", conn)
-print("The top 10 goalkeepers by save percentage in the dataset are:", top_save_percentage_gks.values.tolist())
+stats_df = pd.read_csv('goalkeeper_analysis/data/processed/gkstats_clean.csv')
+print(stats_df.columns)
+print(stats_df.head())
 
-# Finding the correlation between them both 
-correlation = pd.read_sql_query("""
-SELECT 
-    (
-        COUNT(*) * SUM(weekly_wages_clean * "Save Percentage")
-        - SUM(weekly_wages_clean) * SUM("Save Percentage")
-    ) 
-    /
-    (
-        SQRT(
-            (COUNT(*) * SUM(weekly_wages_clean * weekly_wages_clean)
-            - POWER(SUM(weekly_wages_clean), 2))
-            *
-            (COUNT(*) * SUM("Save Percentage" * "Save Percentage")
-            - POWER(SUM("Save Percentage"), 2))
-        )
-    )
-    AS correlation
-FROM gk_combinedsql
-WHERE weekly_wages_clean IS NOT NULL
-AND "Save Percentage" IS NOT NULL;
-""", conn)
+# Checking that Saves and Save Percantage columns are numeric and not strings, if they are strings I will need to convert them to numeric values before I can perform any calculations on them
+print(df[['Player', 'Saves', 'Save Percentage ']].head(26))
 
-print(f"The correlation is: {round(correlation['correlation'].iloc[0], 3)}")
 
